07c0fbc93bf1526cc8e5656bae20b36ff79c25d9
xcarbonell <x.carbonell1dawnuria2020@gmail.com>
2022-04-27 17:52:40 +0200

    crud tasks

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -4,9 +4,17 @@
 
 use App\Task;
 use Faker\Generator as Faker;
+use App\User;
 
 $factory->define(Task::class, function (Faker $faker) {
     return [
         //
+        'title' => $faker->word(5),
+        'desc' => $faker->text(150),
+        'category' => $faker->word(5),
+        'user_id' => rand(1, count(User::all())),
+        'datetask' => '2022-05-01 09:00:00',
+        'created_at' => now(),
+        'updated_at' => now()
     ];
 });
