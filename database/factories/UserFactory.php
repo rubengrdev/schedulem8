90d77c35675d5f1acc6817c72b387839a09383e7
xcarbonell <x.carbonell1dawnuria2020@gmail.com>
2022-04-27 17:52:40 +0200

    crud tasks

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@
 use App\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,10 +20,12 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'name' => $faker->unique()->name(),
+        'email' => $faker->unique()->safeEmail(),
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => Hash::make('00000000'), // password
         'remember_token' => Str::random(10),
+        'created_at' => now(),
+        'updated_at' => now()
     ];
 });
